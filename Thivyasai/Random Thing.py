3d803c4a8dfa1ce09551fd9d3cd000969c2dce0c
Thivyasai.Y <67829748+Thivyasai@users.noreply.github.com>
2020-07-13 10:04:11 -0400

    Add files via upload

---
+++ Thivyasai/Random Thing.py
@@ -0,0 +1,7 @@
+name=input("Enter your name")
+m1=int(input("Enter your cs mark"))
+m2=int(input("Enter your math mark"))
+m3=int(input("Enther your geo mark"))
+tot=m1+m2+m3
+print(name,"secured",tot,"marks")
+print(avg "marks")
