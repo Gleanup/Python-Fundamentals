b956a48873d5d3d77997d61a0288d26879499dcb
abhilashj7 <68013639+abhilashj7@users.noreply.github.com>
2020-07-16 00:57:10 -0400

    Create Assignment1.py

---
+++ Abhilash/Assignment1.py
@@ -0,0 +1,41 @@
+Question 1:
+#The hyphen is not a valid character for variables
+my_message = "Success"
+print (my_message)
+ 
+ 
+Question 2:
+
+num_1 = float(input("Please enter a number: "))
+num_2 = float(input("Please enter another number: "))
+ 
+add = (num_1 + num_2)
+sub = (num_2 - num_1)
+ 
+print ("This is the sum: ",(add))
+print ("This is the difference: ",(sub))
+ 
+ 
+Question 3: 
+
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter another number: "))
+ 
+print (num_1,"#",num_2)
+
+
+Question 4:
+
+the_input = str(input("Please enter Python: "))
+ 
+print (the_input)
+
+
+Question 5:
+
+number_1 = int(input("Please enter a number: "))
+number_2 = (number_1 - 1)
+number_3 = (number_1**3)
+ 
+print ("This is the number minus 1:",(number_2))
+print ("This is the number of the number: ",(number_3))
